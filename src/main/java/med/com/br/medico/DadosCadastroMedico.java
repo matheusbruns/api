29579d9d3a9091a9ddac3c76d1f93ee82876d250
matheusbruns <matheus.rbruns@gmail.com>
2022-12-20 01:03:01 -0300

    criação de requisição POST

---
+++ src/main/java/med/com/br/medico/DadosCadastroMedico.java
@@ -0,0 +1,7 @@
+package med.com.br.medico;
+
+import med.com.br.endereco.DadosEndereco;
+
+public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+
+}
---
+++ src/main/java/med/com/br/medico/Especialidade.java
@@ -0,0 +1,10 @@
+package med.com.br.medico;
+
+public enum Especialidade {
+	
+	ORTOPEDIA,
+	CARDIOLOGIA,
+	GINECOLOGIA,
+	DERMATOLOGIA;
+
+}
